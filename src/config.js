85996cad0033254523cde62dc73d24ac1ccd0303
Đào Phong Trúc Đại <mac@Macs-MacBook-Pro-4.local>
2019-11-02 16:21:20 +0700

    0211

--- src/config.js
+++ src/config.js
@@ -1,2 +1,2 @@
-export  const base_url = process.env.REACT_APP_ENV && process.env.REACT_APP_ENV === 'development' ? "http://localhost:2504":"";
+export  const base_url = process.env.REACT_APP_ENV && process.env.REACT_APP_ENV === 'development' ? "http://localhost:2504":"https://turfcleats-backend.herokuapp.com";
     
